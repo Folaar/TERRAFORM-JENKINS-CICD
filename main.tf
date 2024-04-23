2930755be25e4c533e85b3f261ef14bfb5bafd38
Folaar <133229998+Folaar@users.noreply.github.com>
2024-04-23 17:14:36 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,16 @@
-resource "aws_instance" "Ajay" {
+resource "aws_instance" "folly" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
   user_data              = base64encode(file("website.sh"))
   tags = {
-    Name = "Aj-EC2"
+    Name = "folly's-EC2"
   }
 }
-
 resource "aws_security_group" "ec2_security_group" {
   name        = "ec2 security group"
   description = "allow access on ports 80 and 22 and 443"
-
   ingress {
     description      = "ssh access"
     from_port        = 22
@@ -21,14 +19,12 @@ resource "aws_security_group" "ec2_security_group" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-
   ingress {
     from_port   = 0
     to_port     = 0    # Allow all ports
     protocol    = "-1" # All protocols
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   ingress {
     description      = "https"
     from_port        = 443
@@ -37,7 +33,6 @@ resource "aws_security_group" "ec2_security_group" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-
   ingress {
     description      = "http"
     from_port        = 80
@@ -46,7 +41,6 @@ resource "aws_security_group" "ec2_security_group" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-
   egress {
     from_port        = 0
     to_port          = 0
@@ -54,8 +48,7 @@ resource "aws_security_group" "ec2_security_group" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-
   tags = {
-    Name = "Aj_sg"
+    Name = "folly's"
   }
 }
