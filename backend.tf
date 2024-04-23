b878106f8a712c2badbe587448d024970e2b7ef5
Folaar <133229998+Folaar@users.noreply.github.com>
2024-04-23 17:38:28 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "folasbucksss"   #change name
+    bucket         = "folasbucks"   #change name
     key            = "my-terraform-environment/main"
     region         = "us-east-1"
     dynamodb_table = "mytable"
